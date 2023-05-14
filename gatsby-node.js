751538fa10c00656b3560894ee2005bf64c26fd9
Huáscar Miranda <huascarm@gmail.com>
2023-05-14 19:59:22 -0400

    hidden

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,23 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const methods_1 = require("./methods");
-exports.sourceNodes = ({ actions, createNodeId, createContentDigest }, { substrateNodeUrl, ipfsNodeUrl, spaceIds, seedPhrase }) => __awaiter(void 0, void 0, void 0, function* () {
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }, { substrateNodeUrl, ipfsNodeUrl, spaceIds, seedPhrase }) => {
     try {
-        const api = yield (0, methods_1.createSubsocialApi)({
+        const api = (0, methods_1.createSubsocialApi)({
             substrateNodeUrl,
             ipfsNodeUrl,
             seedPhrase,
+        }).then((api) => {
+            for (const spaceId of spaceIds) {
+                (0, methods_1.getAllDataOfSpace)(api, spaceId).then(({ space, completePosts }) => {
+                    if (!space)
+                        throw new Error("Space not found");
+                    (0, methods_1.pushNode)(api, space, completePosts, actions, createNodeId, createContentDigest);
+                });
+            }
         });
-        for (const spaceId of spaceIds) {
-            const { space, completePosts } = yield (0, methods_1.getAllDataOfSpace)(api, spaceId);
-            if (!space)
-                throw new Error("Space not found");
-            yield (0, methods_1.pushNode)(api, space, completePosts, actions, createNodeId, createContentDigest);
-        }
     }
     catch (error) {
         throw new Error("Error global: " + error.message);
     }
-});
+};
--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,29 +2,30 @@ import { createSubsocialApi, getAllDataOfSpace, pushNode } from "./methods";
 import { ConfigTypes } from "./types";
 import { SourceNodesArgs } from "gatsby";
 
-exports.sourceNodes = async (
+exports.sourceNodes = (
   { actions, createNodeId, createContentDigest }: SourceNodesArgs,
   { substrateNodeUrl, ipfsNodeUrl, spaceIds, seedPhrase }: ConfigTypes
 ) => {
   try {
-    const api = await createSubsocialApi({
+    const api = createSubsocialApi({
       substrateNodeUrl,
       ipfsNodeUrl,
       seedPhrase,
+    }).then((api) => {
+      for (const spaceId of spaceIds) {
+        getAllDataOfSpace(api, spaceId).then(({ space, completePosts }) => {
+          if (!space) throw new Error("Space not found");
+          pushNode(
+            api,
+            space,
+            completePosts,
+            actions,
+            createNodeId,
+            createContentDigest
+          );
+        });
+      }
     });
-
-    for (const spaceId of spaceIds) {
-      const { space, completePosts } = await getAllDataOfSpace(api, spaceId);
-      if (!space) throw new Error("Space not found");
-      await pushNode(
-        api,
-        space,
-        completePosts,
-        actions,
-        createNodeId,
-        createContentDigest
-      );
-    }
   } catch (error: any) {
     throw new Error("Error global: " + error.message);
   }
